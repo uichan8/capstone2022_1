089ed7bed9984bd0dff3be2c59f58144ff255e30
uichan <uichan980202@gmail.com>
2022-06-01 07:38:19 +0900

    update sign algorithm

--- LightUtil/Sign.py
+++ LightUtil/Sign.py
@@ -8,47 +8,42 @@ class Sign(Base):
     sub_led  = [] # 사람 왔을 때 켜져있어야 하는 조명 = 주변 백라이트
 
     main_state = 0
-    sub_state   = 0
+    sub_state  = 0
 
-    max_power =  12*12 #(W)
+    max_power =  12*23 #(W)
 
-    def __init__(self,front:list,middle:list,back:list):
-        for i in front:
-            self.front_led.append(Led(i))
-        for i in middle:
-            self.middle_led.append(Led(i))
-        for i in back:
-            self.back_led.append(Led(i))
+    def __init__(self,main:list,sub:list):
+        for i in main:
+            self.main_led.append(Led(i))
+        for i in sub:
+            self.sub_led.append(Led(i))
 
     @staticmethod
     def led_list_on(list,pwm):
         for led in list:
             led.on(pwm)
 
-    def change_ceilling_state(self,front,mid,back):
+    def change_ceilling_state(self,main,mid):
         for i in range(1,30):
-            front_light = self.front_state + (front - self.front_state)/30*i
-            mid_light = self.mid_state + (mid - self.mid_state)/30*i
-            back_light = self.back_state + (back - self.back_state)/30*i
-            self.led_list_on(self.front_led,int(front_light))
-            self.led_list_on(self.middle_led,int(mid_light))
-            self.led_list_on(self.back_led,int(back_light))
+            main_light = self.main_state + (main - self.main_state)/30*i
+            mid_light  = self.mid_state + (mid - self.mid_state)/30*i
+            self.led_list_on(self.main_led,int(main_light))
+            self.led_list_on(self.sub_led,int(mid_light))
             time.sleep(0.1)
-        self.led_list_on(self.front_led,int(front))
-        self.led_list_on(self.middle_led,int(mid))
-        self.led_list_on(self.back_led,int(back))
-        self.front_state = front
+        self.led_list_on(self.main_led,int(main))
+        self.led_list_on(self.sub_led,int(mid))
+
+        self.main_state = main
         self.mid_state = mid
-        self.back_state = back
     
     def eco_mode(self):
-        self.change_ceilling_state(100,40,10)
+        self.change_ceilling_state(100,40)
     
     def on_mode(self):
-        self.change_ceilling_state(100,100,100)
+        self.change_ceilling_state(100,100)
     
     def off_mode(self):
-        self.change_ceilling_state(0,0,0)
+        self.change_ceilling_state(0,0)
 
     def check_power(self):
-        return (self.front_state + self.mid_state + self.back_led) / 300 * self.max_power #대충한거라 나중에 수정해야함
+        return (self.main_state + self.mid_state) / 300 * self.max_power #대충한거라 나중에 수정해야함
