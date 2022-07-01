017fd4cd7c5620000b1f5a53974ae5cccd069ca3
uichan <uichan980202@gmail.com>
2022-07-01 11:13:50 +0900

    add commands

--- DataBase/DB_manager.py
+++ DataBase/DB_manager.py
@@ -5,11 +5,38 @@ class DB_manager:
     def __init__(self,ip = 'localhost'):
         self.DB = sql.connect(host = ip, port = 3306, user = 'root', password = '1234', db='store')
         self.cursor = self.DB.cursor()
+
+    @staticmethod
+    def time(val = ""):
+        time_data = localtime(time())
+        time_str = str(time_data.tm_mon)+"/" + str(time_data.tm_mday) + " "+str(time_data.tm_hour)+":" + f"{time_data.tm_min:2}" + ":" + f"{time_data.tm_sec:2}" + "/"+ str(val)
+        return time_str
+
+    def update(self, DB_name, val = "",*args):
+        time_str = self.time(val)
+        arg_string = ""
+        for i in args:
+            arg_string += "," + i
+        self.cursor.execute(f"insert into {DB_name} values('{time_str}'{arg_string});")
+        self.DB.commit()
+
+    def read_last(self,DB_name):
+        self.cursor.execute(f"select * from {DB_name} order by datetime desc")
+        result = self.cursor.fetchall()
+        return result
+
+    def clear(self, DB_name):
+        self.cursor.execute(f"delete from {DB_name}")
+        self.DB.commit()
+
+    def command(self,command):
+        self.cursor.execute(command)
     
+
+
     #for temp table
     def update_temp_humi(self, temp, humi):
-        time_data = localtime(time())
-        time_str = str(time_data.tm_mon)+"/" + str(time_data.tm_mday) + " "+str(time_data.tm_hour)+":" + str(time_data.tm_min) + ":" + str(time_data.tm_sec)
+        time_str = self.time()
         self.cursor.execute(f"insert into temp values('{time_str}','{temp}','{humi}');")
         self.DB.commit()
 
---
+++ LightUtil/Led3.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+
+class Led:
+    """
+    led 의 밝기를 조절하는 메소드 입니다.
+
+        input :
+            led_num(int)   : led pin 번호
+            frequency(int) : led 의 주파수를 설정합니다. default = 100
+    """
+    led = None
+    pin_num = 0
+
+    def __init__(self, led_num, frequency = 100):
+        self.pin_num = led_num
+        GPIO.setwarnings(False) #IMPORTANT
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin_num, GPIO.OUT)
+        self.led = GPIO.PWM(self.pin_num, frequency)
+        self.led.start(0)
+
+    def on(self,Brightness = 100):
+        """
+        led를 킬때 쓰는 메소드 입니다.
+
+            input :
+                Brightness(int) : 설정 할 밝기 (0 ~ 100) 을 입력합니다. default = 100
+        """
+        if(0 > Brightness):
+            Brightness = 0
+        if(Brightness > 100):
+            Brightness = 100
+        self.led.ChangeDutyCycle(Brightness)
+
+    def off(self):
+        """
+        led를 끌때 쓰는 메소드 입니다.
+        """
+        self.led.ChangeDutyCycle(0)
