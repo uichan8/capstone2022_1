Led3 fixed, Sign is fixing

LightUtil/Led3.py
@@ -13,7 +13,7 @@ class Led3:
     led = None
     numofpixels = 0
     
-    def __init__(self):
+    def __init__(self,pin_num):
         self.led3_pin = 18
         self.color_array = list(np.zeros((10,3)))
     
@@ -26,28 +26,33 @@ class Led3:
         return Brightness
 
     
-    def set_color(self,r , g ,b, number):
+    def set_color(self, color, number):
         """
         led를 킬때 쓰는 메소드 입니다.
             input :
                 Brightness(int) : 설정 할 밝기 (0 ~ 100) 을 입력합니다. default = 255
         """
-        self.color_array[number] = [self.clipping(r) ,self.clipping(g) ,self.clipping(b)]
+        self.color_array[number] = [self.clipping(color[0]) ,self.clipping(color[1]) ,self.clipping(color[2])]
     
-    def on(self, brightness):
-        for i in range(9,0,-1):
+    def on(self, brightness = 100):
+        for i in range(3,0,-1):
             pixel = neopixel.NeoPixel(board.D18,i)
-            pixel.fill(self.color_array[i])
+            pixel.fill(np.array(self.color_array[i])*brightness/100)
         pixel.show()
        
     def off(self, led3num=0):
         """
         led를 끌때 쓰는 메소드 입니다.
         """
-        self.led3.fill[0, 0, 0]
-        self.led3.show()
+        self.on(0)
         
     def value(self,num=0):
         self.led3.fill(np.array([Brightness_r, Brightness_g, Brightness_b])*num)
+        
+if __name__ == '__main__':
+    a = Led3(1)
+    a.set_color([255,0,0],1)
+    a.on()
+    a.off()
 
     

LightUtil/Sign.py
@@ -1,12 +1,12 @@
 if __name__ == "__main__":
     from Led3 import *
     from Base import *
+    from Led import *
 else:
-    from Led3 import Led3
-    from Base import Base
-import numpy as np
+    from LightUtil.Led3 import *
+    from LightUtil.Base import *
+    from LightUtil.Led import *
 import time
-import neopixel
 
 class Sign(Base):
     """간판의 조명을 조절하는 클래스 입니다.
@@ -14,73 +14,60 @@ class Sign(Base):
             main(list) : 간판 매인 조명의 조명의 입력 핀들의 번호들을 입력 합니다.(항상 켜져 있어야 하는 조명)
             sub(list)  : 간판 중간라인의 조명의 입력 핀들의 번호들을 입력 합니다.
         """
-    main1_led = [] # 항상 켜져있어야 하는 조명 = 중앙에 kookmin 에 붙어있는 조명
-    main2_led = [] # 항상 켜져있어야 하는 조명 = 중앙에 kookmin 에 붙어있는 조명
+    main_led = [] # 항상 켜져있어야 하는 조명 = 중앙에 kookmin 에 붙어있는 조명
     sub_led  = [] # 사람 왔을 때 켜져있어야 하는 조명 = 주변 백라이트
+    
+    color1 = [0,255,25]
+    color2 = [255,150,0]
 
-    main1_state = 0
-    main2_state = 0 
+    main_state = 0
     sub_state  = 0
 
     max_power =  12*23 #(W)
 
-    def __init__(self,main1:list, main2:list ,sub:list):
-        for i in main1:
-            self.main1_led.append(Led3(i))
-        for i in main2:
-            self.main2_led.append(Led3(i))
+    def __init__(self,main:list,sub:list):
+        for i in main:
+            self.main_led.append(Led3(i))
         for i in sub:
-            self.sub_led.append(Led3(i))
+            self.sub_led.append(Led(i))
+            
+        self.main_led[0].set_color(self.color1,1)
+        self.main_led[0].set_color(self.color2,2)
 
     @staticmethod
-    def led_list_on(list, pwm):
+    def led_list_on(list,pwm):
         for led in list:
             led.on(pwm)
-    def led_list_on_main1(list,Brightness_r, Brightness_g, Brightness_b, number):
-        for main1_led in list:
-            led_main1.on(Brightness_r, Brightness_g, Brightness_b, number)
-    def led_list_on_main2(list,Brightness_r, Brightness_g, Brightness_b, number):
-        for main2_led in list:
-            led_main2.on(Brightness_r, Brightness_g, Brightness_b, number)   
 
-    def change_sign_state(self, main1_value, main2_value, sub):          
-        for main1_led in list:
-            main1_led.value(main1_value)
-        for main2_led in list:
-            main2_led.value(main2_value)
-            
-       # self.main1_led.fill(np.array([Brightness_r, Brightness_g, Brightness_b])*main1_value)
-        #self.main2_led.fill(np.array([Brightness_r, Brightness_g, Brightness_b])*main2_value)
+    def change_ceilling_state(self,main,sub):
         for i in range(1,30):
+            main_light = self.main_state + (main - self.main_state)/30*i
             sub_light  = self.sub_state + (sub - self.sub_state)/30*i
+            self.led_list_on(self.main_led,int(main_light))
             self.led_list_on(self.sub_led,int(sub_light))
             time.sleep(0.01)
-        self.led_list_on(self.sub_led,int(sub_light))
-        #self.led_list_on_main(self.main1_led, int(Brightness_r, Brightness_g, Brightness_b, number))
-
-        time.sleep(0.01)
-
+        self.led_list_on(self.main_led,int(main))
         self.led_list_on(self.sub_led,int(sub))
 
-
-        self.main_state = 1
-        self.main_state = 1
+        self.main_state = main
         self.sub_state = sub
-    #def changd_sign_state_main(self, 
+    
     def eco_mode(self):
         """매장 간판 조명을 절약하는 모드입니다."""
-        self.change_sign_state(0.5,0.5,0)
-        self.led3.show()
+        self.change_ceilling_state(40,0)
     
     def on_mode(self):
         """매장 간판 조명을 켜는 모드입니다."""
-        self.change_sign_state(1,1,100)
-        self.led3.show()
-
+        self.change_ceilling_state(100,100)
+    
     def off_mode(self):
         """매장 간판 조명을 끌때 쓰는 메소드 입니다."""
-        self.change_sign_state(0,0,0)
-        self.led3.show()
+        self.change_ceilling_state(0,0)
 
     def check_power(self):
-        return (self.main_state + self.sub_state) / 300 * self.max_power #대충한거라 나중에 수정해야함
+        return (self.main_state + self.sub_state) / 300 * self.max_power #대충한거라 나중에 수정해야함 나중에 수정해야함
+    
+if __name__ == '__main__':
+    a = Sign([18],[16])
+    a.on_mode()
+    a.eco_mode()